91932dee2cbb7ed5a97f074d4fa926c154ec89c3
Shin-sibainu <shincode0712@gmail.com>
2021-08-26 16:13:57 +0900

    modifed thread style

--- src/components/main/thread/Thread.jsx
+++ src/components/main/thread/Thread.jsx
@@ -3,19 +3,56 @@ import React from "react";
 export const Thread = () => {
   return (
     <div className="thread">
-      <strong>No1：PC勢FPS値高いから撃ち負けるんだがwww</strong>
+      <strong>
+        <span id="threadnumber">No.1:</span>
+        PC勢FPS値高いから撃ち負けるんだがwww
+      </strong>
       <p className="threadComment">
-        PS4勢涙目。こっちが上手くても壁張かえられるから絶対に負けるくそげーすぎるwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww
-        PS4勢涙目。こっちが上手くても壁張かえられるから絶対に負けるくそげーすぎる
-        PS4勢涙目。こっちが上手くても壁張かえられるから絶対に負けるくそげーすぎる
-        PS4勢涙目。こっちが上手くても壁張かえられるから絶対に負けるくそげーすぎる
+        <p id="username">
+          No.1 名前: <b> ShinCode</b>
+          <span className="threadInfo">
+            2021/08/26(木)14:29
+            <a href="#">[返信]</a>
+          </span>
+        </p>
+        <span id="threadContent">
+          PS4勢涙目。こっちが上手くても壁張かえられるから絶対に負けるくそげーすぎるwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww
+          PS4勢涙目。こっちが上手くても壁張かえられるから絶対に負けるくそげーすぎる
+          PS4勢涙目。こっちが上手くても壁張かえられるから絶対に負けるくそげーすぎる
+          PS4勢涙目。こっちが上手くても壁張かえられるから絶対に負けるくそげーすぎる
+        </span>
       </p>
-      <div className="threadInfo">
-        <b>ShinCode</b>
-        <span>No.1</span>
-        2021/08/26(Thu)14:29
-        <a href="#">[返信]</a>
-      </div>
+      {/* 返信用 */}
+      <form>
+        <table className="replyTable">
+          <tbody>
+            <tr>
+              <th>おなまえ</th>
+              <td>
+                <input
+                  type="text"
+                  name="name"
+                  maxLength="12"
+                  className="nameInput"
+                />
+              </td>
+            </tr>
+            <tr>
+              <th colSpan="2">
+                このスレッドに書き込む
+                <br />
+                <textarea name="comment" className="textArea"></textarea>
+                {/*   <input
+                      type="submit"
+                      value="投稿"
+                      className="submitButton"
+                    /> */}
+                <button className="submitButton">投稿（仮）</button>
+              </th>
+            </tr>
+          </tbody>
+        </table>
+      </form>
     </div>
   );
 };
